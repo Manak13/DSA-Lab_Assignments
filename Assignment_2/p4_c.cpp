f011b3d08e07e4cf9c609abf9833a4e30515d63a
Manak13 <manakkatyal@gmail.com>
2025-09-15 09:08:01 +0530

    Update p4_c.cpp

--- Assignment_2/p4_c.cpp
+++ Assignment_2/p4_c.cpp
@@ -1,29 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-    
-    int n = 6;
-    char arr[5] = {'H','e','l','l','o'};
-    
-    // arr[5] = {'H','e','l','l','o'};
-    for(int i=0; i<n; i++){
-        char check = arr[i];
-        for(int j=i+1; j<n;){
-            if(arr[j] == check) {
-                for(int k=j; k<n-1; k++){
-                    arr[k] = arr[k+1];
-                }
-                n--;
-            }
-            else j++;
+int main() {
+    string str, result = "";
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    for (char ch : str) {
+        // check if not a vowel
+        if (!(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
+              ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')) {
+            result += ch;  
         }
     }
-    
 
-    // Printing the reversed string
-    for(int i=0; i<n; i++){
-        cout << arr[i];
-    }
+    cout << "String without vowels: " << result << endl;
     return 0;
 }
