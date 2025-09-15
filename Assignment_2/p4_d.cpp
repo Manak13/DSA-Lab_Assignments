03a09485062aeaaccd48696d7a08735a15de01cf
Manak13 <manakkatyal@gmail.com>
2025-09-15 09:10:04 +0530

    Update p4_d.cpp

--- Assignment_2/p4_d.cpp
+++ Assignment_2/p4_d.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-int main(){
-    
-    const int n = 5;
-    char arr[5] = {'h','e','l','l','o'};
-    
-    // Arranging the string in alphabetical order
-    for(int i=0; i<n-1; i++){
-        for(int j=0; j<n-1-i; j++){
-            if(arr[j]>arr[j+1]){
-                char temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
+int main() {
+    int n;
+    cout << "Enter number of strings: ";
+    cin >> n;
+
+    string arr[100]; 
+    cout << "Enter the strings:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
-    // Printing the sorted string
-    for(int i=0; i<n; i++){
-        cout << arr[i];
+    
+    sort(arr, arr + n);
+
+    cout << "Strings in alphabetical order:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << endl;
     }
+
     return 0;
 }
